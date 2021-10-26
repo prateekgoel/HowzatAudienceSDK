c06d9e4385b9d4bda8f8065079cbda3a203e60ef
Subbu <subrahmanyam@jungleegames.com>
2021-10-26 12:26:11 +0530

    Initial commit

---
+++ Example/HowzatAudienceSDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HowzatAudienceSDK
+//
+//  Created by jungleesubbu on 10/26/2021.
+//  Copyright (c) 2021 jungleesubbu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
